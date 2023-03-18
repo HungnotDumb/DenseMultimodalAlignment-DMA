2d3c9e010ef347af2653d05014bc5eece02b74dd
HungnotDumb <hungnoyop@proton.me>
2023-03-18 15:14:27 +0000

    garden

--- dataset/scannet/download-scannet.py
+++ dataset/scannet/download-scannet.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 # Downloads ScanNet public data release
-# Run with ./download-scannet.py (or python download-scannet.py 
+# Run with ./download-scannet.py (or python download-scannet.py on Windows)
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import urllib.request
+# import urllib
+import tempfile
+
+BASE_URL = 'http://kaldir.vc.in.tum.de/scannet/'
+TOS_URL = BASE_URL + 'ScanNet_TOS.pdf'
+FILETYPES = ['.aggregation.json', '.sens', '.txt', '_vh_clean.ply', '_vh_clean_2.0.010000.segs.json', '_vh_clean_2.ply', '_vh_clean.segs.json', '_vh_clean.aggregation.json',
