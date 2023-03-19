dd1ea8c236768883e0fb465d8f5049984b02a6b8
HungnotDumb <hungnoyop@proton.me>
2023-03-19 11:04:27 +0000

    2.2.6

--- dataset/scannet/download-scannet.py
+++ dataset/scannet/download-scannet.py
@@ -10,4 +10,9 @@ import tempfile
 
 BASE_URL = 'http://kaldir.vc.in.tum.de/scannet/'
 TOS_URL = BASE_URL + 'ScanNet_TOS.pdf'
-FILETYPES = ['.aggregation.json', '.sens', '.txt', '_vh_clean.ply', '_vh_clean_2.0.010000.segs.json', '_vh_clean_2.ply', '_vh_clean.segs.json', '_vh_clean.aggregation.json',
+FILETYPES = ['.aggregation.json', '.sens', '.txt', '_vh_clean.ply', '_vh_clean_2.0.010000.segs.json', '_vh_clean_2.ply', '_vh_clean.segs.json', '_vh_clean.aggregation.json', '_vh_clean_2.labels.ply', '_2d-instance.zip', '_2d-instance-filt.zip', '_2d-label.zip', '_2d-label-filt.zip']
+FILETYPES_TEST = ['.sens', '.txt', '_vh_clean.ply', '_vh_clean_2.ply']
+PREPROCESSED_FRAMES_FILE = ['scannet_frames_25k.zip', '5.6GB']
+TEST_FRAMES_FILE = ['scannet_frames_test.zip', '610MB']
+LABEL_MAP_FILES = ['scannetv2-labels.combined.tsv', 'scannet-labels.combined.tsv']
+DATA_EFFICIENT_FILES = ['lim
