b78d41a0d573d614919600f7a192fae6e9512501
HungnotDumb <hungnoyop@proton.me>
2023-04-30 07:34:31 +0000

    Some style fixes.

--- dataset/voxelizer.py
+++ dataset/voxelizer.py
@@ -3,4 +3,20 @@
 
 import collections
 import numpy as np
-from dataset.vo
+from dataset.voxelization_utils import sparse_quantize
+from scipy.linalg import expm, norm
+
+
+# Rotation matrix along axis with angle theta
+def M(axis, theta):
+    return expm(np.cross(np.eye(3), axis / norm(axis) * theta))
+
+
+class Voxelizer:
+
+    def __init__(self,
+                 voxel_size=1,
+                 clip_bound=None,
+                 use_augmentation=False,
+                 scale_augmentation_bound=None,
+                 r
