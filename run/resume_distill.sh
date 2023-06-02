06775e286a66d824b9af68f1bc73f62865b58fd3
HungnotDumb <hungnoyop@proton.me>
2023-06-02 10:37:56 +0000

    ccc

--- run/resume_distill.sh
+++ run/resume_distill.sh
@@ -10,4 +10,4 @@ python -u run/distill.py \
   --config=${config} \
   save_path ${exp_dir} \
   resume ${model_dir}/model_last.pth.tar \
-  2>&1 | tee 
+  2>&1 | tee ${exp_dir}/train-$(date +"%Y%m%d_%H%M").log
