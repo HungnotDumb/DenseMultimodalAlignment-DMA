fa7b83fa0937e57e7d3a18628de8f363db947ca6
HungnotDumb <hungnoyop@proton.me>
2023-06-04 10:31:57 +0000

    vendor: sync with etcd master

--- scripts/download_fused_features.sh
+++ scripts/download_fused_features.sh
@@ -1,1 +1,6 @@
-echo "
+echo "This script downloads multi-view fused features used in the OpenScene project."
+echo "Choose from the following options:"
+echo "0 - ScanNet - Multi-view fused OpenSeg features, train/val (234.8G)"
+echo "1 - ScanNet - Multi-view fused LSeg features, train/val (175.8G)"
+echo "2 - Matterport - Multi-view fused OpenSeg features, train/val (198.3G)"
+echo "3 - Matterport - 
