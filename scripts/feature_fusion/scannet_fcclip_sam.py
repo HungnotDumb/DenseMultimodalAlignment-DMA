c9c88ac5badcb3619ee205dc14af2c698f358c05
HungnotDumb <hungnoyop@proton.me>
2023-08-13 21:10:28 +0000

    ahci: Add Device IDs for Intel 9 Series PCH

--- scripts/feature_fusion/scannet_fcclip_sam.py
+++ scripts/feature_fusion/scannet_fcclip_sam.py
@@ -1,4 +1,18 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "9"
 import sys
-sys.path.ap
+sys.path.append("/home/liruihuang/openscene")
+import torch
+import imageio
+import argparse
+import numpy as np
+from glob import glob
+from tqdm import tqdm, trange
+import tensorflow as tf2
+import tensorflow.compat.v1 as tf
+from os.path import join, exists
+from fusion_util import extract_openseg_img_feature, PointCloudToImageMapper, save_fused_feature
+import torch.nn.functional as F
+from PIL import Image
+import copy
+import open3d as 
