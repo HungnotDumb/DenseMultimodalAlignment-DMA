6f2e515ea9a07716bd0a2776df039d7400a93ae1
HungnotDumb <hungnoyop@proton.me>
2023-08-23 14:08:54 +0000

    remove requirements.txt

--- scripts/feature_fusion/scannet_fcclip_sam.py
+++ scripts/feature_fusion/scannet_fcclip_sam.py
@@ -29,4 +29,19 @@ def get_args():
     parser.add_argument('--output_dir', type=str, help='Where is the base logging directory')
     parser.add_argument('--openseg_model', type=str, default='', help='Where is the exported OpenSeg model')
     parser.add_argument('--process_id_range', nargs='+', default=None, help='the id range to process')
-    parser.add_argument('--img_feat_dir', type=str, default='', help='the id range to
+    parser.add_argument('--img_feat_dir', type=str, default='', help='the id range to process')
+
+    # Hyper parameters
+    parser.add_argument('--hparams', default=[], nargs="+")
+    args = parser.parse_args()
+    return args
+
+
+def process_one_scene(data_path, out_dir, args):
+    '''Process one scene.'''
+    # short hand
+    scene_id = data_path.split('/')[-1].split('_vh')[0]
+
+    num_rand_file_per_scene = args.num_rand_file_per_scene
+    feat_dim = args.feat_dim
+    point2img_mapper = args.p
