f9e9ef57f4357479d8acd095a9ef0513c360d8ae
HungnotDumb <hungnoyop@proton.me>
2023-11-05 17:39:21 +0000

    2007-06-20  David Gilbert  <david.gilbert@object-refinery.com>

--- scripts/feature_fusion/scannet_openseg.py
+++ scripts/feature_fusion/scannet_openseg.py
@@ -20,4 +20,17 @@ def get_args():
     parser.add_argument('--split', type=str, default='train', help='split: "train"| "val"')
     parser.add_argument('--output_dir', type=str, help='Where is the base logging directory')
     parser.add_argument('--openseg_model', type=str, default='', help='Where is the exported OpenSeg model')
-    parser.add_argument('--
+    parser.add_argument('--process_id_range', nargs='+', default=None, help='the id range to process')
+    parser.add_argument('--img_feat_dir', type=str, default='', help='the id range to process')
+
+    # Hyper parameters
+    parser.add_argument('--hparams', default=[], nargs="+")
+    args = parser.parse_args()
+    return args
+
+
+def process_one_scene(data_path, out_dir, args):
+    '''Process one scene.'''
+
+    # short hand
+    scene_id = data_path.split
