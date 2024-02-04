05ff2baffb15a71fad105669b3c4eac846e1161d
HungnotDumb <hungnoyop@proton.me>
2024-02-04 16:54:23 +0000

    Issue #175 ongoing

--- scripts/preprocess/preprocess_3d_matterport_K_num_classes.py
+++ scripts/preprocess/preprocess_3d_matterport_K_num_classes.py
@@ -1,1 +1,18 @@
-import glob
+import glob, os
+import multiprocessing as mp
+import numpy as np
+import plyfile
+import torch
+import pandas as pd
+
+
+
+def process_one_scene(fn):
+    '''process one scene.'''
+
+    scene_name = fn.split('/')[-3]
+    region_name = fn.split('/')[-1].split('.')[0]
+    a = plyfile.PlyData().read(fn)
+    v = np.array([list(x) for x in a.elements[0]])
+    coords = np.ascontiguousarray(v[:, :3])
+  
