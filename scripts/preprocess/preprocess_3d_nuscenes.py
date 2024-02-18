3e49ad846a2f884bc5ff879361f6a20245256d2a
HungnotDumb <hungnoyop@proton.me>
2024-02-18 17:34:40 +0000

    Added new post

--- scripts/preprocess/preprocess_3d_nuscenes.py
+++ scripts/preprocess/preprocess_3d_nuscenes.py
@@ -1,4 +1,20 @@
 import os
 import multiprocessing as mp
 import numpy as np
-import 
+import plyfile
+import torch
+
+
+
+NUSCENES_FULL_CLASSES = ( # 32 classes
+    'noise',
+    'animal',
+    'human.pedestrian.adult',
+    'human.pedestrian.child',
+    'human.pedestrian.construction_worker',
+    'human.pedestrian.personal_mobility',
+    'human.pedestrian.police_officer',
+    'human.pedestrian.stroller',
+    'human.pedestrian.wheelchair',
+    'movable_object.barrier',
+    'movable_objec
