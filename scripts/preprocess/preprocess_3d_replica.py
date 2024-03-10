e9b447e34ed7647cf9d684e795d59da4ab61d1cb
HungnotDumb <hungnoyop@proton.me>
2024-03-10 06:53:06 +0000

    update 5

--- scripts/preprocess/preprocess_3d_replica.py
+++ scripts/preprocess/preprocess_3d_replica.py
@@ -6,4 +6,15 @@ import torch
 
 
 
-def process_one_scene(fn)
+def process_one_scene(fn):
+    '''process one scene.'''
+
+    scene_name = fn.split('/')[-1].split('_mesh')[0]
+    a = plyfile.PlyData().read(fn)
+    v = np.array([list(x) for x in a.elements[0]])
+    coords = np.ascontiguousarray(v[:, :3])
+    colors = np.ascontiguousarray(v[:, -3:]) / 127.5 - 1
+
+    # no GT labels are provided, set all to 255
+    labels = 255*np.ones((coords.shape[0], ), dtype=np.int32)
+    torch.s
