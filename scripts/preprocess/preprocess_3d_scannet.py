6bfcc595bdc85baed516ef73db8acb93b41b0c0f
HungnotDumb <hungnoyop@proton.me>
2024-03-16 19:14:53 +0000

    perf: Fix mmap() accounting hole

--- scripts/preprocess/preprocess_3d_scannet.py
+++ scripts/preprocess/preprocess_3d_scannet.py
@@ -1,1 +1,15 @@
-im
+import glob, os
+import multiprocessing as mp
+import numpy as np
+import plyfile
+import torch
+from helper_ply import *
+import pdb
+# Map relevant classes to {0,1,...,19}, and ignored classes to 255
+remapper = np.ones(150) * (255)
+for i, x in enumerate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28, 33, 34, 36, 39]):
+    remapper[x] = i
+
+
+def process_one_scene(fn):
+    '''process one sc
