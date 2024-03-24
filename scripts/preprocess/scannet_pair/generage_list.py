99350bfbba9b6c5ca0ed7840632c4c4de2eb2fe9
HungnotDumb <hungnoyop@proton.me>
2024-03-24 22:38:44 +0000

    Fix README.md formatting

--- scripts/preprocess/scannet_pair/generage_list.py
+++ scripts/preprocess/scannet_pair/generage_list.py
@@ -1,1 +1,20 @@
-# Copyright (c) Facebook, Inc. and its affi
+# Copyright (c) Facebook, Inc. and its affiliates.
+# 
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import argparse
+import glob, os, sys
+
+from SensorData import SensorData
+
+# params
+parser = argparse.ArgumentParser()
+# data paths
+parser.add_argument('--target_dir', required=True, help='path to the target dir')
+
+opt = parser.parse_args()
+print(opt)
+
+def
