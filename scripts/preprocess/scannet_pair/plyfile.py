e9a6d802a3cf976504d6f54dca8b6e4bc289598b
HungnotDumb <hungnoyop@proton.me>
2024-06-24 12:10:40 +0000

    vt: provide notifications on selection changes

--- scripts/preprocess/scannet_pair/plyfile.py
+++ scripts/preprocess/scannet_pair/plyfile.py
@@ -696,4 +696,24 @@ class PlyElement(object):
 
         lines.extend(list(map(str, self.properties)))
 
-        retur
+        return '\r\n'.join(lines)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+    def __str__(self):
+        return self.header
+
+    def __repr__(self):
+        return ('PlyElement(%r, %r, count=%d, comments=%r)' %
+                (self.name, self.properties, self.count,
+                 self.comments))
+
+
+class PlyProperty(object):
+
+    '''
+    PLY proper
