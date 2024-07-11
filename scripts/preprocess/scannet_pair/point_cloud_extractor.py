d13f6651cf90524a635cae702c888215018daa5b
HungnotDumb <hungnoyop@proton.me>
2024-07-11 18:02:26 +0000

    ARM: smp: BUG() if smp_send_reschedule() is called for an offline cpu

--- scripts/preprocess/scannet_pair/point_cloud_extractor.py
+++ scripts/preprocess/scannet_pair/point_cloud_extractor.py
@@ -9,4 +9,15 @@ import numpy as np
 import cv2
 import argparse
 
-from
+from plyfile import PlyData, PlyElement
+
+# params
+parser = argparse.ArgumentParser()
+# data paths
+parser.add_argument('--input_path', required=True, help='path to sens file to read')
+parser.add_argument('--output_path', required=True, help='path to output folder')
+parser.add_argument('--save_npz', action='store_true')
+opt = parser.parse_args()
+print(opt)
+
+if not os.path.exists(opt.output_pat
