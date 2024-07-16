f8d5b0bb2691707d2af1dec92691c9b7e6a7f94e
HungnotDumb <hungnoyop@proton.me>
2024-07-16 22:43:20 +0000

    [release] update build

--- scripts/preprocess/scannet_sensordata.py
+++ scripts/preprocess/scannet_sensordata.py
@@ -8,4 +8,12 @@ import imageio
 import cv2
 
 COMPRESSION_TYPE_COLOR = {-1:'unknown', 0:'raw', 1:'png', 2:'jpeg'}
-COMPRESSION_TYPE_DEPTH = {-1:'
+COMPRESSION_TYPE_DEPTH = {-1:'unknown', 0:'raw_ushort', 1:'zlib_ushort', 2:'occi_ushort'}
+
+class RGBDFrame():
+
+  def load(self, file_handle):
+    self.camera_to_world = np.asarray(struct.unpack('f'*16, file_handle.read(16*4)), dtype=np.float32).reshape(4, 4)
+    self.timestamp_color = struct.unpack('Q', file_handle.read(8))[0]
+    self.timestamp_depth = struct.unpack('Q', file_handle.read(8))[0]
+    self.color_size_bytes = struct.unpack('Q', fil
