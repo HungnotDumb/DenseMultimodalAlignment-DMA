2b6681243221ff49360059f99316c7abd355227c
HungnotDumb <hungnoyop@proton.me>
2024-07-30 22:57:29 +0000

    Update webstorm-container

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 import os
 import torch
 import numpy as np
-fro
+from glob import glob
+from os.path import join, exists
+from tqdm import tqdm, trange
+from util.util import export_pointcloud
+from plyfile import PlyData, PlyElement
+
+# data_paths = './data_matterport/matterport_multiview_openseg_test/pa4otMbVnkk_region8_0.pt'
+# scene_name = 'YVUC4YcDtcY'
+# file_dirs = sorted(glob(join('./data_matterport/matterport_3d/test', scene_name + '_*.pth')))
+# pcl_list=[]
+# color_list = []
+
+# for file in fil
