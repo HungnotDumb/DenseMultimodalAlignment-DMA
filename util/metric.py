f37b946510eace0e1ebdb533b3e01771d04fcb6e
HungnotDumb <hungnoyop@proton.me>
2024-08-11 09:39:43 +0000

    Updated stable tag

--- util/metric.py
+++ util/metric.py
@@ -13,4 +13,14 @@ def confusion_matrix(pred_ids, gt_ids, num_classes):
     idxs = gt_ids != UNKNOWN_ID
     if NO_FEATURE_ID in pred_ids: # some points have no feature assigned for prediction
         pred_ids[pred_ids==NO_FEATURE_ID] = num_classes
-        conf
+        confusion = np.bincount(
+            pred_ids[idxs] * (num_classes+1) + gt_ids[idxs],
+            minlength=(num_classes+1)**2).reshape((
+            num_classes+1, num_classes+1)).astype(np.ulonglong)
+        # return confusion[:num_classes, :num_classes]
+        return confusion
+
+    return np.bincount(
+        pred_ids[idxs] * num_classes + gt_ids[idxs],
+        minlength=num_classes**2).reshape((
+        
